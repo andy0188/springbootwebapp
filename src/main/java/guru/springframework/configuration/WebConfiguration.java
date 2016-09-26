4290686516fb6a92cc2caf3a014f88957361bb9f
chenwenbo <594017622@qq.com>
2016-09-26 09:39:23 +0800

    init project

--- src/main/java/guru/springframework/configuration/WebConfiguration.java
+++ src/main/java/guru/springframework/configuration/WebConfiguration.java
@@ -3,9 +3,8 @@ package guru.springframework.configuration;
 import org.h2.server.web.WebServlet;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class WebConfiguration {
     @Bean
     ServletRegistrationBean h2servletRegistration(){
